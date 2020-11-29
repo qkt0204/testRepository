ee52c4e0d994a5c7581b21f7fc0881e2c59377c4
qkt0204 <qkt0204@vip.163.com>
2020-11-29 19:23:29 +0800

    git commit

---
+++ src/java/com/qkt/springboot/Application.java
@@ -0,0 +1,12 @@
+package com.qkt.springboot;
+
+/**
+ * ClassName: Application <br/>
+ * Description: <br/>
+ * date: 2020/11/29 19:20<br/>
+ *
+ * @author 馨儿<br />
+ * @since JDK 1.8
+ */
+public class Application {
+}
